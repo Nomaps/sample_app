dc95c8e96e189e86b46ce06a39ea584d7cbb4d4c
Nomaps <Nomaps@gmail.com>
2012-06-03 15:18:13 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '708301c1722230c4238819e187859debcfb6c5173442b3066d2c01032ec2ee262ab3e177247dde5bfe6b7fd2aae03735b716ae0c184047cadd73010d9a8e1814'
