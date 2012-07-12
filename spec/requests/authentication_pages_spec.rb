65b5daccd3583695da7bf1b4c63f1390396fe40c
Nomaps <Nomaps@gmail.com>
2012-07-12 01:11:28 +0100

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -31,20 +31,20 @@ describe "Authentication" do
   	let(:user) { FactoryGirl.create(:user) }
 	  	before { sign_in user }
 
-	it { should have_selector('title', text: user.name) }
+  	it { should have_selector('title', text: user.name) }
 
-  it { should have_link('Users',       href: users_path) }
-	it { should have_link('Profile',     href: user_path(user)) }
-  it { should have_link('Settings',    href: edit_user_path(user)) }
-	it { should have_link('Sign out',    href: signout_path) }
-	it { should_not have_link('Sign in', href: signin_path) }
+    it { should have_link('Users',       href: users_path) }
+  	it { should have_link('Profile',     href: user_path(user)) }
+    it { should have_link('Settings',    href: edit_user_path(user)) }
+  	it { should have_link('Sign out',    href: signout_path) }
+  	it { should_not have_link('Sign in', href: signin_path) }
 
     describe "followed by signout" do
       before { click_link "Sign out" }
       it { should have_link('Sign in') }
     end
-	end
- end
+  end
+end
  
  describe "authorization" do
 
@@ -115,4 +115,17 @@ describe "Authentication" do
       end
     end
  end
+
+ describe "in the Microposts controller" do
+
+    describe "submitting to the create action" do
+      before { post microposts_path }
+      specify { response.should redirect_to(signin_path) }
+    end
+
+    describe "submitting to the destroy action" do
+      before { delete micropost_path(FactoryGirl.create(:micropost)) }
+      specify { response.should redirect_to(signin_path) }
+    end
+  end
 end
